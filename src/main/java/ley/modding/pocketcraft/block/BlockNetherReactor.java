6ce9cfaac0f11ab52ead8f797a62310abcf55a3d
Timo Ley <timo@leyt.xyz>
2021-04-06 09:33:28 +0200

    Add content

---
+++ src/main/java/ley/modding/pocketcraft/block/BlockNetherReactor.java
@@ -0,0 +1,21 @@
+package ley.modding.pocketcraft.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.creativetab.CreativeTabs;
+
+public class BlockNetherReactor extends Block {
+
+    public BlockNetherReactor() {
+        super(Material.iron);
+        setBlockName("reactor");
+        setCreativeTab(CreativeTabs.tabDecorations);
+    }
+
+    @Override
+    public void registerBlockIcons(IIconRegister reg) {
+        blockIcon = reg.registerIcon("reactor");
+    }
+
+}
---
+++ src/main/java/ley/modding/pocketcraft/block/BlockStonecutter.java
@@ -0,0 +1,41 @@
+package ley.modding.pocketcraft.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.util.IIcon;
+
+public class BlockStonecutter extends Block {
+
+    private IIcon top;
+    private IIcon side;
+    private IIcon side2;
+    private IIcon bottom;
+
+    public BlockStonecutter() {
+        super(Material.rock);
+        setBlockName("stonecutter");
+        setCreativeTab(CreativeTabs.tabDecorations);
+    }
+
+    @Override
+    public IIcon getIcon(int dir, int meta) {
+        switch (dir) {
+            case 0: return bottom;
+            case 1: return top;
+            case 2:
+            case 3:
+                return side;
+        }
+        return side2;
+    }
+
+    @Override
+    public void registerBlockIcons(IIconRegister reg) {
+        top = reg.registerIcon("stonecutter_top");
+        side = reg.registerIcon("stonecutter_side");
+        side2 = reg.registerIcon("furnace_side");
+        bottom = reg.registerIcon("furnace_top");
+    }
+}
